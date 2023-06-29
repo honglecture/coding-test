ea7dd0746cda5ec63536e0c7c6ae478c0012ab7c
홍성철 <honglecture@ibizsoftware.net>
2023-06-29 10:44:15 +0900

    commit

---
+++ programmers/lv0/181952/Solution.java
@@ -0,0 +1,11 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181952
+// 코딩테스트 연습 > 코딩 기초 트레이닝 > 문자열 출력하기
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);
+    }
+}
