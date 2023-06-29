43ef67ccbcd1c9f18127cdcecf395a6199943f55
홍성철 <honglecture@ibizsoftware.net>
2023-06-29 09:54:03 +0900

    commit

--- baekjoon/2000/2557/Main.java
+++ baekjoon/2000/2557/Main.java
@@ -1,5 +1,14 @@
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n1 = Integer.parseInt(bf.readLine());
+        int n2 = Integer.parseInt(bf.readLine());
+        int result = n1 + n2;
+        bw.write(result+"");
+        bw.flush();
+        bw.close();
     }
 }
